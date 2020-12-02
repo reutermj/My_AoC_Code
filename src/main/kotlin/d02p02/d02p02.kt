6a4cb139c405d42b93728381c2ad644802e5c974
mark <github@markreuter.org>
2020-12-02 15:05:44 -0600

    Finished day 2

--- src/main/kotlin/d02p02/d02p02.kt
+++ src/main/kotlin/d02p02/d02p02.kt
@@ -5,8 +5,8 @@ import java.io.File
 fun main(args: Array<String>) {
     var numPassingPasswords = 0
     File(args[0]).forEachLine { line ->
-        val (times, rule, password) = line.split(" ")
-        val (pos1, pos2) = times.split("-").map { it.toInt() - 1 }
+        val (positions, rule, password) = line.split(" ")
+        val (pos1, pos2) = positions.split("-").map { it.toInt() - 1 }
         if ((password[pos1] == rule[0]) xor (password[pos2] == rule[0]))
             numPassingPasswords++
     }
