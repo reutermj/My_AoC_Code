4437743f88662cd697cbd19cbf8101dfef6c5baf
mark <github@markreuter.org>
2020-12-02 15:08:01 -0600

    Finished day 2

--- src/main/kotlin/d02p01/d02p01.kt
+++ src/main/kotlin/d02p01/d02p01.kt
@@ -5,8 +5,8 @@ import java.io.File
 fun main(args: Array<String>) {
     var numPassingPasswords = 0
     File(args[0]).forEachLine { line ->
-        val (times, rule, password) = line.split(" ")
-        val (low, high) = times.split("-").map { it.toInt() }
+        val (range, rule, password) = line.split(" ")
+        val (low, high) = range.split("-").map { it.toInt() }
         val occurrences = password.fold(0) { acc, c -> acc + if (c == rule[0]) 1 else 0 }
         if (occurrences in low..high)
             numPassingPasswords++
